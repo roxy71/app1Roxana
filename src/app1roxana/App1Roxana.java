cfe6cb4d0d8bdcd313eead5df43f359f167e7f4b
roxy71 <roxana71janet@gmail.com>
2017-10-25 21:05:12 -0300

    Este es el ejercicio de variables

---
+++ src/app1roxana/App1Roxana.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1roxana;
+
+/**
+ *
+ * @author LAB03
+ */
+public class App1Roxana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero1;
+        int numero2;
+        
+        numero1=40;
+        numero2=25;
+         System.out.println("Suma de numero1 y numero2: "+(numero1+numero2));
+          System.out.println("Multiplicacion de numero1 y numero2: "+(numero1*numero2));
+           System.out.println("Resta de numero1 y numero2: "+(numero1-numero2));
+        System.out.println("Division de numero1 y numero2: "+(numero1/numero2));
+         System.out.println("Residuo de numero1 y numero2: "+(numero1%numero2));
+    }
+    
+}
